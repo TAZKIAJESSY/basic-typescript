585650ce67137cccb70bfc17bde9efd8a7ddc9f4
tazkiajessy <jessy566eee@gmail.com>
2021-03-29 16:54:19 +0200

    typescript in object

--- index.ts
+++ index.ts
@@ -5,13 +5,30 @@ function sum(a: number, b: number) {
 const c = sum(2, 4);
 // console.log("summ", c);
 
+// typescript in Array
 function multiplyArrayByTen(array: number[]) {
   return array.map((element) => (element *= 10));
 }
 
 const numbers = [1, 2, 3, 4];
 const numbersMultiplyByTen = multiplyArrayByTen(numbers);
-console.log(numbersMultiplyByTen);
+// console.log(numbersMultiplyByTen);
+
+// typescript in OBJECT
+type person = {
+  firstName: string;
+  lastName: string;
+};
+
+function fullName({ firstName, lastName }: person) {
+  return firstName + " " + lastName;
+}
+
+const firstName = "tazkia";
+const lastName = "jessy";
+
+const getName = fullName({ firstName, lastName });
+console.log(getName);
 
 //npm install ts-node
 // npm install typescript --include=dev
